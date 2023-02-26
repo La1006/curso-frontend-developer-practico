55ac4883168c8fbdc1a9aba37d7f2b6b977d0827
Eduardo Alcántara <inglalofi@gmail.com>
2023-02-26 13:25:01 -0600

    Add the Shopping cart

--- main.js
+++ main.js
@@ -1,8 +1,29 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const burguerMenu = document.querySelector(".menu");
+const cartMenu = document.querySelector(".navbar-shopping-cart");
+const mobileMenu = document.querySelector(".mobile-menu");
+const aside = document.querySelector(".product-detail");
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+burguerMenu.addEventListener('click', toggleMobileMenu);
+cartMenu.addEventListener('click', toggleCartAside);
 
 function toggleDesktopMenu(){
+    aside.classList.add('inactive');
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu(){
+    aside.classList.add('inactive');
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCartAside(){
+    desktopMenu.classList.add('inactive');
+    mobileMenu.classList.add('inactive');
+    aside.classList.toggle('inactive');
+
+
 }
